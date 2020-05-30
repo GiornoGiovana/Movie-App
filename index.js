0a965333ecd7e9768e8c4cf0da7d7823249a35b1
giornogiovana <drakeb2008@hotmail.com>
2020-05-29 23:57:41 -0500

    Basic functionality

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const API_KEY = "13ded7d2";
 
 const movie = document.querySelector("[data-input-movie]");
-const year_mov = document.querySelector("[data-input-year]");
 const btn = document.querySelector("[data-button]");
 const movieTitle = document.querySelector("[data-title-movie]");
 const movieImage = document.querySelector("[data-image-movie]");
@@ -10,11 +9,16 @@ const moviePlot = document.querySelector("[data-plot-movie]");
 
 btn.addEventListener("click", (event) => {
   event.preventDefault();
-  getData(movie.value, year_mov.value);
+  getData(movie.value);
+  movie.value = "";
 });
 
-const getData = async (movie, year) => {
-  const URL = `http://www.omdbapi.com/?apikey=${API_KEY}&t=${movie}&y=${year}&plot`;
+window.onload = () => {
+  getData();
+};
+
+const getData = async (movie = "Joker") => {
+  const URL = `http://www.omdbapi.com/?apikey=${API_KEY}&t=${movie}&plot`;
   const response = await fetch(URL);
   const data = await response.json();
   console.log(data);
