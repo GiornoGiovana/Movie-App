126edd811cdff26d584295c297cd2b38a06bf89f
giornogiovana <drakeb2008@hotmail.com>
2020-06-02 18:04:34 -0500

    Add styles and restrinctions

--- index.js
+++ index.js
@@ -10,22 +10,20 @@ const movieList = document.querySelector("[data-movie-list]");
 movieBtn.addEventListener("click", () => {
   const title = movieInput.value;
   const type = movieOption.options[movieOption.selectedIndex].value;
-  fetchData(title, type).then((res) => {
-    movieList.innerHTML = "";
-    res.forEach((movieData, index) => {
-      if (movieData.Poster !== "N/A" && index < 9) {
-        const moviCont = temp.content.cloneNode(true); //clonas los elemento del template
-        moviCont.querySelector("[data-movie-title]").innerHTML =
-          movieData.Title;
-        moviCont
-          .querySelector("[data-movie-image]")
-          .setAttribute("src", movieData.Poster);
-        moviCont.querySelector("[data-movie-year]").innerHTML = movieData.Year;
 
-        movieList.appendChild(moviCont);
+  if (movieInput.value !== "") {
+    fetchData(title, type).then((movie) => {
+      if (movie.Response === "False") {
+        alert("Movie or Series not found, please try again!");
+      } else {
+        sendDataTemplate(movie);
       }
     });
-  });
+  } else {
+    alert("Type a movie or series!");
+  }
+
+  movieInput.value = "";
 });
 
 const fetchData = async (title = "", type = "") => {
@@ -34,5 +32,21 @@ const fetchData = async (title = "", type = "") => {
   );
   const data = await response.json();
   console.log(data);
-  return data.Search;
+  return data.Response === "True" ? data.Search : data;
+};
+
+const sendDataTemplate = (movie) => {
+  movieList.innerHTML = "";
+  movie.forEach((movieData, index) => {
+    if (movieData.Poster !== "N/A" && index < 8) {
+      const moviCont = temp.content.cloneNode(true); //clonas los elemento del template
+      moviCont.querySelector("[data-movie-title]").innerHTML = movieData.Title;
+      moviCont
+        .querySelector("[data-movie-image]")
+        .setAttribute("src", movieData.Poster);
+      moviCont.querySelector("[data-movie-year]").innerHTML = movieData.Year;
+
+      movieList.appendChild(moviCont);
+    }
+  });
 };
