b4276160bce78114b4e115af2f25b422e62b5eaa
giornogiovana <drakeb2008@hotmail.com>
2020-06-02 19:28:51 -0500

    Get the selected movie data

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ movieBtn.addEventListener("click", () => {
         alert("Movie or Series not found, please try again!");
       } else {
         sendDataTemplate(movie);
+        dropMovieInfo(movieList);
       }
     });
   } else {
@@ -40,6 +41,7 @@ const sendDataTemplate = (movie) => {
   movie.forEach((movieData, index) => {
     if (movieData.Poster !== "N/A" && index < 8) {
       const moviCont = temp.content.cloneNode(true); //clonas los elemento del template
+      moviCont.getElementById("movieID").innerHTML = movieData.imdbID;
       moviCont.querySelector("[data-movie-title]").innerHTML = movieData.Title;
       moviCont
         .querySelector("[data-movie-image]")
@@ -49,4 +51,25 @@ const sendDataTemplate = (movie) => {
       movieList.appendChild(moviCont);
     }
   });
+};
+
+const dropMovieInfo = (movies) => {
+  if (movies.hasChildNodes()) {
+    for (let i = 0; i < movies.childElementCount; i++) {
+      const movie = movies.children[i];
+      movie.addEventListener("click", () => {
+        let myMovieID = movie.children[0].textContent;
+        console.log(myMovieID);
+        fetchSelectedMovie(myMovieID).then((mov) => console.log(mov));
+      });
+    }
+  }
+};
+
+const fetchSelectedMovie = async (id) => {
+  const response = await fetch(
+    `http://www.omdbapi.com/?apikey=${API_KEY}&i=${id}`
+  );
+  const data = await response.json();
+  return data;
 };
